457cc89150029044b51d6c1057faaadeb79293b0
Kate <kit.ty.kate@disroot.org>
2021-12-21 21:06:16 +0000

    tmp

--- lib/build.ml
+++ lib/build.ml
@@ -127,10 +127,7 @@ module Op = struct
 
   module Outcome = Current.String
 
-  let lwt_pool = Lwt_pool.create 200 (fun () -> Lwt.return_unit)
-
   let run { config = { connection; timeout }; master; urgent; base } job { Key.pool; commit; variant; ty } () =
-    Lwt_pool.use lwt_pool @@ fun () ->
     let master = Current_git.Commit.hash master in
     let build_spec ~for_docker =
       let base = base_to_string base in
